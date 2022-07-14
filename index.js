c9951e0846caf76208934c7361528508af096133
Michael Okello <okellofrost@gmail.com>
2022-07-14 18:29:18 +0300

    finalizing

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function Circle(radius){
     this.radius = radius ;
     this.draw = function(){
         console.log('draw');
-    }
+    };
 }
 
 const anotherCircle = new Circle(1);
@@ -52,7 +52,78 @@ function Circle2(radius){
 }
 const circle2 = new Circle2(23);
 
+//to enumerate all the properties use for in
 for ( let key in circle2){
     //console.log('Enum',key);
+
+    if( typeof circle2[key] !== 'function')
     console.log(key , circle2[key]);
 }
+
+///another approach of displaying the keys in an object
+// To get all keys in an object use Object method
+const keys = Object.keys(circle2);
+console.log(keys);
+//to check if some properties are in a method
+
+if('radius' in circle2)
+console.log('Circle has a radius');
+
+
+//Introducing some complexity in our circle object
+// This is where we introduce abstraction to our implemmentation
+
+function Circle3(radius){
+    this.radius = radius ;
+
+ let defaultLocation ={
+      x : 0,
+      y : 0
+    };
+
+  let computeOptimumLocation = function(factor){
+
+    };
+
+    this.draw = function(){
+        computeOptimumLocation(0.1);
+        console.log('draw');
+    };
+}
+
+const anotherCircle2 = new Circle3(1);
+
+//Getters & Setters
+function Circle4(radius){
+    this.radius = radius ;
+
+ let defaultLocation = {
+      x : 0,
+      y : 0
+    };
+
+    this.getDefaultLocation = function(){
+        return defaultLocation;
+    }
+
+    this.draw = function(){
+ 
+        console.log('draw');
+    };
+
+    Object.defineProperty(this,'defaultLocation',{
+    get : function(){
+        return defaultLocation;
+    },
+    set : function(value){
+        if(!value.x || value.y)
+        throw new Error('Invlid Location');
+
+        defaultLocation = value;
+    }
+  });
+}
+
+const anotherCircle3 = new Circle4(12);
+
+anotherCircle3.defaultLocation = 1;
